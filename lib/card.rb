6e9d9eee9981398783ea506d929b39bdff330bd4
App Academy Student <student@appacademy.io>
2017-02-01 17:40:44 -0800

    write card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,3 +1,18 @@
 class Card
+  attr_reader :value, :suit
 
+  def initialize(value, suit)
+    @value = value
+    @suit = suit
+  end
+
+  def compare(other)
+    my_val = VALUE_LIST.index(value)
+    other_val = VALUE_LIST.index(other.value)
+    my_val <=> other_val
+  end
+
+  private
+
+  VALUE_LIST = %i(one two three four five six seven eight nine ten jack queen king ace)
 end
