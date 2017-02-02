985fa4b035c82b71c7387d4f5b9ce2bbd74d50f7
scott <ssny88@gmail.com>
2017-02-01 18:12:27 -0800

    add spec for draw method in deck class

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -22,13 +22,24 @@ describe Deck do
   end
 
   describe '#shuffle' do
-    it "returns the deck in a random order"
+    let(:other_deck) { Deck.new.populate }
+    before(:each) do
+      deck.populate
+      deck.shuffle
+    end
+
+    it "returns the deck in a random order" do
+      expect(deck.card_stack).to match_array(other_deck.card_stack)
+      expect(deck.card_stack).not_to eq(other_deck.card_stack)
+    end
 
     it "modifies the deck instance variable"
-  end
 
   describe '#draw' do
-    it "removes the top card off the deck"
+    it "removes the top card off the deck" do
+      deck.draw
+      expect(deck.card_stack.size).to eq(51)
+    end
 
     it "returns the top card"
   end
