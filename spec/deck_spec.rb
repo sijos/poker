004720cdeccfadf3191a356af392f25a9186516c
App Academy Student <student@appacademy.io>
2017-02-01 17:19:41 -0800

    create pending specs for Deck class

---
+++ spec/deck_spec.rb
@@ -0,0 +1,26 @@
+require 'deck'
+
+describe Deck do
+  describe '#initialize' do
+    it "initializes with an empty deck"
+  end
+
+  describe '#populate' do
+    it "creates a new deck of size 52"
+
+    it "only contains card objects"
+  end
+
+  describe '#shuffle' do
+    it "returns the deck in a random order"
+
+    it "modifies the deck instance variable"
+  end
+
+  describe '#draw' do
+    it "removes the top card off the deck"
+
+    it "returns the top card"
+  end
+
+end
